d7496f0fd5554f1e4d2dc8b3366e66bb99449eeb
Ethan Loewen <ethanloewen@gmail.com>
2022-04-06 01:26:39 +0000

    Complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.sum(:annual_revenue)
+puts Store.average(:annual_revenue)
+
+
+# puts Store.where('annual_revenue >=': 1000000).size
+# Store.joins(:annual_revenue).where('annual_revenue >=': 1000000).count
+puts Store.where('annual_revenue >= ?', 1000000).count
